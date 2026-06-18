6556652c1aafda7ad29424376d04cdde6968962a
kinley-tenzin <kinleykaks@gmail.com>
2026-06-19 02:37:14 +0600

    build

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
+  base: "/143-.--/",
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+
